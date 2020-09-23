1b10a0d8f408283ccc4a11b9c3f33afe19ab712b
Bryan Huizi <bryanhuizi@gmail.com>
2020-09-22 21:03:51 -0700

    3.2 Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,14 +1,17 @@
 fn main() {
-    let mut x = 5;
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    
+    let (x, y, z ) = tup;
+
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+    
+    println!("The value at pos 1 is: {}", tup.0);
+    println!("The value at pos 2 is: {}", tup.1);
+    println!("The value at pos 3 is: {}", tup.2);
 
-    let y = 5;
-
-    let y = y + 1;
-
-    let y = y * 2;
-
-    println!("The value of y is {}", y);
+    let a: [i32;  5] = [1, 2, 3, 4, 5];
+    println!("The value at pos 0 is: {}", a[0])
 }
