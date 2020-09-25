6080b27cb774c98aee63e9ccada649aa461788d3
Bryan Huizi <bryanhuizi@gmail.com>
2020-09-24 20:14:14 -0700

    3.5 Control Flow

---
+++ expressions/src/_.main.rs
@@ -0,0 +1,15 @@
+fn main(){
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let next_number = 5;
+
+    if next_number != 0 {
+        println!("number was something other then zero");
+    }
+}
