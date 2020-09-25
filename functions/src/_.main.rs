ef5d1cf2ab956f87dd77e8deacaed45ea33c5f78
Bryan Huizi <bryanhuizi@gmail.com>
2020-09-24 19:24:55 -0700

    3.3 Functions

---
+++ functions/src/_.main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let _x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y in main is: {}", y);
+
+    another_function(5, 6);
+}
+
+fn another_function(x: i32, y: i32){
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
